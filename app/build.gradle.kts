a8adb35f0f623d5a80fda2c130769f4857db14d7
Rahul Sharma <v9store41@gmail.com>
2026-05-20 12:05:00 +0530

    Update Gradle plugin versions for Android and Kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
-
 android {
     namespace = "com.liquidglass.launcher"
     compileSdk = 34
